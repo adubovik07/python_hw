6ac17780c71a5731937d05228dbed431f192ddf9
Anastasia Dubovik <a.dubovik2010@yandex.ru>
2023-06-02 23:48:57 +0700

    add tasks 16, 18, 20

---
+++ main.py
@@ -0,0 +1,32 @@
+# print("Введите что-нибудь: ")
+# a = int(input())
+
+# b = int(input("Введите еще что-нибудь: "))
+
+# print(a, "+", b, "=", a + b)
+
+# a = 6.7686
+# b = 7.87987
+# print(a*b)
+# print(round(a*b,3))
+
+# username = input("Введите имя: ")
+# if username == "Маша" :
+#     print("Ура, это же МАША")
+# elif username == "Марина" :
+#     print("Я так ждала Вас, Марина!")
+# elif username == "Ильнар" :
+#     print("Ильнар - топ)")
+# else:
+#     print("Привет, ", username)
+
+# n = 123
+# summa = 0
+# while n > 0:
+#     x = n % 10
+#     summa
+# summa + x
+# n = n // 10
+# print(summa) * 9
+
+
